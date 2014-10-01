b910cdaace162332ac8d46ef4d00199b76c2d496
Arjan Topolovec <arjan.top@gmail.com>
2014-10-01 13:59:04 +0200

    Implemented exponential backoff for sending gcm messages.

--- gcm/retrysender.go
+++ gcm/retrysender.go
@@ -1,11 +1,16 @@
 package gcm
 
 import (
-	"code.google.com/p/go.net/context"
+	"time"
 
+	"code.google.com/p/go.net/context"
 	gcmlib "github.com/alexjlockwood/gcm"
+	"github.com/cenkalti/backoff"
+
 	"github.com/opentarock/service-api/go/util/contextutil"
 )
+
+const maxRetries = 10
 
 type RetrySender struct {
 	sender *gcmlib.Sender
@@ -20,8 +25,24 @@ func NewRetrySender(apiKey string) *RetrySender {
 }
 
 func (s *RetrySender) SendMessage(ctx context.Context, msg *gcmlib.Message) error {
-	return contextutil.Do(ctx, func() error {
-		_, err := s.sender.SendNoRetry(msg)
-		return err
+	b := backoff.NewExponentialBackOff()
+	b.InitialInterval = 10 * time.Millisecond
+	ticker := backoff.NewTicker(b)
+	cancel := func() { ticker.Stop() }
+	return contextutil.DoWithCancel(ctx, cancel, func() error {
+		count := 0
+		var err error
+		for _ = range ticker.C {
+			if count == maxRetries {
+				return err
+			}
+			_, err = s.sender.SendNoRetry(msg)
+			if err == nil {
+				cancel()
+				break
+			}
+			count++
+		}
+		return nil
 	})
 }
